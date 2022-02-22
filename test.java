9088e1b1d59cc231d10752ddd4c24f4e1ab443c5
mattballito <99840943+mattballito@users.noreply.github.com>
2022-02-22 13:14:20 -0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,15 @@
+package com.Richturtle7.bots;
+
+
+
+public class test{
+
+    public void onStart(){
+        System.out.println(":)");
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("lol ;)");
+    }
+}
